b62bda794ff65755342d1223595c72e4a9b9933a
Rodrigo Nogueira <rdg6design@gmail.com>
2021-02-27 04:56:25 -0300

    clearing

--- portscanv1.py
+++ portscanv1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from termcolor import colored
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,9 +9,9 @@ host = "192.168.0.2"
 
 def portscanner(port):
         if sock.connect_ex((host, port)):
-            print("Port %d is closed" % (port))
+            print (colored("[!!] Port %d is closed" % (port), 'red'))
         else:
-            print("Port %d is opened" % (port))
+            print (colored("[+] Port %d is opened" % (port), 'green'))
 
-for port in range(1,100):
+for port in range(1,4000):
     portscanner(port)    
